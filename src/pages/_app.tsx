c213b40db1c849c131507f4e15e1fe552b29be0b
Thiago Cedran <thiagocdn@gmail.com>
2020-12-29 14:04:43 -0300

    Finished page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import { AnimateSharedLayout } from 'framer-motion'
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
+  )
 }
 
 export default MyApp
